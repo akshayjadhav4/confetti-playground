2797c08a08cafbf3aa97de7c6d6b35f10c78286d
Akshay Jadhav <akshayjadhav461999@gmail.com>
2024-12-27 20:50:25 +0530

    Pan Gesture working-V1

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import Playground from "@/components/Playground";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const Home = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Playground />
-    </SafeAreaView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Playground />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
 
--- components/PlaygroundTile.tsx
+++ components/PlaygroundTile.tsx
@@ -1,6 +1,6 @@
 import { ORIGIN } from "@/constants/dimensions";
 import React, { useEffect } from "react";
-import { Image } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 import Animated, {
   Easing,
@@ -9,6 +9,7 @@ import Animated, {
   withRepeat,
   withTiming,
   cancelAnimation,
+  withSpring,
 } from "react-native-reanimated";
 type Props = {
   top: number;
@@ -33,6 +34,9 @@ const PlaygroundTile = (props: Props) => {
   const right = useSharedValue(ORIGIN.x);
   const left = useSharedValue(ORIGIN.x);
   const size = useSharedValue(0);
+  const translationX = useSharedValue(0);
+  const translationY = useSharedValue(0);
+
   const tileAnimatedStyle = useAnimatedStyle(() => {
     return {
       top: top.value,
@@ -42,6 +46,45 @@ const PlaygroundTile = (props: Props) => {
       transform: [{ rotate: `${rotateAngle.value}deg` }],
     };
   }, []);
+
+  const tileImage = useAnimatedStyle(() => {
+    return {
+      height: size.value,
+      width: size.value,
+      transform: [
+        { translateX: translationX.value },
+        { translateY: translationY.value },
+      ],
+    };
+  });
+
+  const pan = Gesture.Pan()
+    .minDistance(1)
+    .onStart(() => {})
+    .onUpdate((event) => {
+      // Convert degrees to radians
+      const angleInRadians = (rotateAngle.value * Math.PI) / 180;
+      /**
+       * consider rotation of the tile when updating its translation
+       * rotatedX = translationX * cos(angle) + translationY * sin(angle);
+       * rotatedY = -translationX * sin(angle) + translationY * cos(angle);
+       */
+      const rotatedX =
+        event.translationX * Math.cos(angleInRadians) +
+        event.translationY * Math.sin(angleInRadians);
+
+      const rotatedY =
+        -event.translationX * Math.sin(angleInRadians) +
+        event.translationY * Math.cos(angleInRadians);
+
+      translationX.value = rotatedX;
+      translationY.value = rotatedY;
+    })
+    .onEnd(() => {
+      translationX.value = withSpring(0);
+      translationY.value = withSpring(0);
+    })
+    .runOnJS(true);
 
   useEffect(() => {
     rotateAngle.value = withRepeat(
@@ -81,16 +124,19 @@ const PlaygroundTile = (props: Props) => {
         tileAnimatedStyle,
       ]}
     >
-      <Image
-        source={images[props.iconPath]}
-        style={{
-          height: "100%",
-          width: "100%",
-          borderRadius: props.cornerRadius,
-          position: "absolute",
-          resizeMode: "cover",
-        }}
-      />
+      <GestureDetector gesture={pan}>
+        <Animated.Image
+          source={images[props.iconPath]}
+          style={[
+            {
+              borderRadius: props.cornerRadius,
+              position: "absolute",
+              resizeMode: "cover",
+            },
+            tileImage,
+          ]}
+        />
+      </GestureDetector>
     </Animated.View>
   );
 };
